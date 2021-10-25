99343c658c73f45b79c27e7e1ea9b2ea50e6b210
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2021-10-24 21:07:07 -0300

    2.9.1.1

--- version_control.py
+++ version_control.py
@@ -1,11 +1,15 @@
 from bs4 import BeautifulSoup
 import httpx
 
-LOCAL_VERSION = '2.9.1'
+LOCAL_VERSION = '2.9.1.1';
 
 def get_online_version():
     soup = BeautifulSoup(httpx.get('https://github.com/ewertonhm/cmdSA/releases').text, 'lxml')
-    div = soup.find('div', {"class": "release-header"})
-    online_version = div.find('a').text
-    if online_version != LOCAL_VERSION:
-        print("Uma nova versão do script está disponível para baixar em: https://github.com/ewertonhm/cmdSA/releases")
+    try:
+        div = soup.find('div', {"class": "Box-body"})
+        online_version = div.find('a').text
+        if online_version != LOCAL_VERSION:
+            print(
+                "Uma nova versão do script está disponível para baixar em: https://github.com/ewertonhm/cmdSA/releases")
+    except Exception:
+        print("Não foi possível verificar se existem atualizações disponíveis")
