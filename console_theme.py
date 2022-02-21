a1237533bbf537083ccb40c74554f161a8cc1f1a
Ewerton Henrique Marschalk <ewertonmarschalk@gmail.com>
2022-02-21 10:21:01 -0300

    Delete console_theme.py

--- console_theme.py
+++
@@ -1,9 +0,0 @@
-from rich.theme import Theme
-from rich.console import Console
-
-custom_theme = Theme({
-    "warning": "yellow",
-    "disaster": "bold red"
-})
-
-CONSOLE = Console(theme=custom_theme)
